1dfc63a27a513039d30035d6e5d55669dd46122b
jpbatz <jph@hawaii.edu>
2015-03-06 11:52:35 -1000

    MBA commit for c9

--- gol.js
+++ gol.js
@@ -1,6 +1,29 @@
 (document).ready(function() {
 
-  var cell = $('div').html("Hello");
-    $(".grid").appendChild(cell);
+  var cell = $('div', {
+    class: "cell"
+  });
+
+  $(".grid").append(cell);
+
+  this.grid = $('<div class="grid">');
+
+  var width = 12;
+  var height = 12;
+  // create grid with dimensions provided by 
+  // the arguments width and height
+  for(var i=0; i<height; i++) {
+    var gridRowCell = $('<div>', {
+      class: "grid_cell row"
+    });
+
+    for(var j=0; j<width; j++) {
+      var gridColCell = $('<div>', {
+        class: "grid_cell col"
+      });
+      gridRowCell.append(gridColCell);
+    }
+    this.grid.append(gridRowCell);
+  }
 
 });
