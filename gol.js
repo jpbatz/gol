a54a35970398b8976ea003995186725c61e2e05e
jpbatz <jph@hawaii.edu>
2015-03-08 01:22:06 -1000

    MBA includes logic to determine cell status for next generation

--- gol.js
+++ gol.js
@@ -53,13 +53,12 @@ $(document).ready(function() {
   for(var row1=0; row1<cells.length; row1++) {
     for(var col1=0; col1<cells[row1].length; col1++) {
       var randomValue = Math.floor(Math.random() * 2);
-      // console.log(randomValue);
       var randomColor = "white";
       if(randomValue === 0) {
         randomColor = "white";
         cells[row1][col1].alive = false;
       } else if(randomValue === 1){
-        randomColor = "black";
+        randomColor = "purple";
         cells[row1][col1].alive = true;
       }
       cells[row1][col1].domDiv.style.backgroundColor = randomColor;
@@ -69,69 +68,90 @@ $(document).ready(function() {
   // count live neighbors for cell at (x,y)
   for(var row2=0; row2<cells.length; row2++) {
     for(var col2=0; col2<cells[row2].length; col2++) {
-      var number_of_neighbors = countNeighbors(row2,col2);
-      console.log("neighbor_count for (" + row2 + "," + col2 + ") = " + number_of_neighbors);
+      var number_of_live_neighbors = countNeighbors(row2,col2);
+      console.log("neighbor_count for (" + row2 + "," + col2 + ") = " + number_of_live_neighbors);
+      cells[row2][col2].num_of_live_neighbors = number_of_live_neighbors;
+
+      // determine next generation status and set
+      var nextGenStatus;
+
+      if(cells[row2][col2].alive === true) {
+        if(number_of_live_neighbors < 2) {
+          nextGenStatus = false;        // 
+        } else if(number_of_live_neighbors === 2 || number_of_live_neighbors ===3) {
+          nextGenStatus = true;
+        } else if(number_of_live_neighbors > 3) {
+          nextGenStatus = false;
+        }
+      } else if(cells[row2][col2].alive === false) {
+        if(number_of_live_neighbors === 3) {
+          nextGenStatus = true;
+        }
+      }
+      cells[row2][col2].next_gen = nextGenStatus;
     }
+
   }
+ 
 
 
-function countNeighbors(r,c) {
+  function countNeighbors(r,c) {
 
-  var neighbor_count = 0;
-  var x = r;
-  var y = c;
+    var neighbor_count = 0;
+    var x = r;
+    var y = c;
 
-  if(x !== 0 && y !== 0) {
-    if(cells[x-1][y-1].alive === true) {
-      neighbor_count++;
+    if(x !== 0 && y !== 0) {
+      if(cells[x-1][y-1].alive === true) {
+        neighbor_count++;
+      }
     }
-  }
 
-  if(y !== 0) {
-    if(cells[x][y-1].alive === true) {
-      neighbor_count++;
+    if(y !== 0) {
+      if(cells[x][y-1].alive === true) {
+        neighbor_count++;
+      }
     }
-  }
 
-  if(x !== cells.length-1 && y !== 0) {
-    if(cells[x+1][y-1].alive === true) {
-      neighbor_count++;
+    if(x !== cells.length-1 && y !== 0) {
+      if(cells[x+1][y-1].alive === true) {
+        neighbor_count++;
+      }
     }
-  }
 
-  if(x !== 0) {
-    if(cells[x-1][y].alive === true) {
-      neighbor_count++;
+    if(x !== 0) {
+      if(cells[x-1][y].alive === true) {
+        neighbor_count++;
+      }
     }
-  }
 
-  if(x !== cells.length-1) {
-    if(cells[x+1][y].alive === true) {
-      neighbor_count++;
+    if(x !== cells.length-1) {
+      if(cells[x+1][y].alive === true) {
+        neighbor_count++;
+      }
     }
-  }
 
-  if(x !== 0 && y !== cells[x].length-1) {
-    if(cells[x-1][y+1].alive === true) {
-      neighbor_count++;
+    if(x !== 0 && y !== cells[x].length-1) {
+      if(cells[x-1][y+1].alive === true) {
+        neighbor_count++;
+      }
     }
-  }
 
-  if(y !== cells[x].length-1) {
-    if(cells[x][y+1].alive === true) {
-      neighbor_count++;
+    if(y !== cells[x].length-1) {
+      if(cells[x][y+1].alive === true) {
+        neighbor_count++;
+      }
     }
-  }
 
-  if(x !== cells.length-1 && y !== cells[x].length-1) {
-    if(cells[x+1][y+1].alive === true) {
-      neighbor_count++;
+    if(x !== cells.length-1 && y !== cells[x].length-1) {
+      if(cells[x+1][y+1].alive === true) {
+        neighbor_count++;
+      }
     }
+
+    return neighbor_count;
+
   }
-
-  return neighbor_count;
-
-}
 
 
 });
