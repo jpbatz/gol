f6ef98642e634ed4b1f13a6b20891855b32c943d
jpbatz <jph@hawaii.edu>
2015-03-07 17:45:47 -1000

    MBA greates grid and cell objects with connection to the DOM

--- gol.js
+++ gol.js
@@ -1,29 +1,70 @@
-(document).ready(function() {
+var cells = []; // contains rows []
 
-  var cell = $('div', {
-    class: "cell"
-  });
+$(document).ready(function() {
 
-  $(".grid").append(cell);
+  var cell = {
+    // cell_no: 0,
+    alive: false,
+    domDiv: ""
+    // location: {
+    //   x: 0,       // row
+    //   y: 0        // col
+    // },
+    // live_neighbors = {
+    //   n1:,
+    //   n2:,
+    //   n3:,
+    //   n4:,
+    //   n5:,
+    //   n6:,
+    //   n7:,
+    //   n8:
+    // },
+    // num_of_live_neighbors: function() { return ... },
+    // next_gen: function() { return ...; }
+  };
 
-  this.grid = $('<div class="grid">');
-
+  // grid dimensions - to be specified by user input
   var width = 12;
   var height = 12;
-  // create grid with dimensions provided by 
-  // the arguments width and height
-  for(var i=0; i<height; i++) {
+
+  // bind 'grid_div' as a property
+  var grid = $('#grid_div');
+  // bind 'controls' as a property
+  // this.controls = $('<div id="controls_div">');
+
+  // create grid with dimensions provided by arguments width and height
+  // goal:
+  //  [
+  //    [ {cell}, {cell}, {cell}, {cell},...],
+  //    [ {cell}, {cell}, {cell}, {cell},...],
+  //            ...
+  //    [ {cell}, {cell}, {cell}, {cell},...]
+  //  ]
+  for(var i=0; i<width; i++) {
+    // [                    ]
     var gridRowCell = $('<div>', {
       class: "grid_cell row"
     });
 
-    for(var j=0; j<width; j++) {
+    var row = [];   // creates a new row each iteration, contains cells {}
+
+    for(var j=0; j<height; j++) {
+      // [ {}, {}, {}, {},...]
       var gridColCell = $('<div>', {
         class: "grid_cell col"
       });
       gridRowCell.append(gridColCell);
+      row.push(
+        {
+          alive: false,
+          domDiv: gridColCell.get(0)
+        });
     }
-    this.grid.append(gridRowCell);
+    grid.append(gridRowCell);
+    cells.push(row);
   }
+
+  console.log(cells);
 
 });
