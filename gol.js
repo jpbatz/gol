5f5af9277b9e57144fdadff5de26b3477dad77d9
jpbatz <jph@hawaii.edu>
2015-03-09 11:49:05 -1000

    MBA commit for c9

--- gol.js
+++ gol.js
@@ -1,8 +1,11 @@
-var cells = []; // contains rows []
+// var cells = []; // contains rows []
 
 $(document).ready(function() {
+  
+  var cells = []; // contains rows []
 
   var cell = {
+    cell_num: 0,
     alive: false,
     domDiv: "",
     num_of_live_neighbors: 0,
@@ -10,23 +13,23 @@ $(document).ready(function() {
   };
 
   // grid dimensions - to be specified by user input
-  var width = 12;
-  var height = 12;
+  var height = 12;  // height = rows = y
+  var width = 12;   // width = cols = x
 
   // bind 'grid_div' as a property
   var grid = $('#grid_div');
   // bind 'controls' as a property
   // this.controls = $('<div id="controls_div">');
 
-  // create grid with dimensions provided by arguments width and height
-  // goal:
+  // build grid with dimensions provided by arguments height and width
   //  [
   //    [ {cell}, {cell}, {cell}, {cell},...],
   //    [ {cell}, {cell}, {cell}, {cell},...],
   //            ...
   //    [ {cell}, {cell}, {cell}, {cell},...]
   //  ]
-  for(var i=0; i<width; i++) {
+  var cellNum = 1;
+  for(var i=0; i<height; i++) {
     // [                    ]
     var gridRowCell = $('<div>', {
       class: "grid_cell row"
@@ -34,20 +37,24 @@ $(document).ready(function() {
 
     var row = [];   // creates a new row each iteration, contains cells {}
 
-    for(var j=0; j<height; j++) {
+    for(var j=0; j<width; j++) {
       var gridColCell = $('<div>', {
         class: "grid_cell col"
       });
       gridRowCell.append(gridColCell);
       row.push(
         {
+          cell_num: cellNum,
           alive: false,
           domDiv: gridColCell.get(0)
         });
+      cellNum++;
     }
     grid.append(gridRowCell);
     cells.push(row);
   }
+
+  console.log("***** " + Number(cellNum-1) + " CELLS *****");
 
   // generate random seed
   for(var row1=0; row1<cells.length; row1++) {
@@ -56,50 +63,55 @@ $(document).ready(function() {
       var randomColor = "white";
       if(randomValue === 0) {
         randomColor = "white";
-        cells[row1][col1].alive = false;
+        cells[col1][row1].alive = false;
       } else if(randomValue === 1){
         randomColor = "purple";
-        cells[row1][col1].alive = true;
+        cells[col1][row1].alive = true;
       }
-      cells[row1][col1].domDiv.style.backgroundColor = randomColor;
+      cells[col1][row1].domDiv.style.backgroundColor = randomColor;
+      cells[col1][row1].domDiv.innerHTML = cells[col1][row1].cell_num;
     }
   }
 
   // count live neighbors for cell at (x,y)
   for(var row2=0; row2<cells.length; row2++) {
     for(var col2=0; col2<cells[row2].length; col2++) {
-      var number_of_live_neighbors = countNeighbors(row2,col2);
-      console.log("neighbor_count for (" + row2 + "," + col2 + ") = " + number_of_live_neighbors);
-      cells[row2][col2].num_of_live_neighbors = number_of_live_neighbors;
+      var number_of_live_neighbors = countNeighbors(col2,row2);
+      console.log("live neighbor count for (" + col2 + "," + row2 + ") = " + number_of_live_neighbors);
+      cells[col2][row2].num_of_live_neighbors = number_of_live_neighbors;
 
       // determine next generation status and set
       var nextGenStatus;
 
-      if(cells[row2][col2].alive === true) {
+      if(cells[col2][row2].alive === true) {
         if(number_of_live_neighbors < 2) {
-          nextGenStatus = false;        // 
-        } else if(number_of_live_neighbors === 2 || number_of_live_neighbors ===3) {
+          nextGenStatus = false;
+          console.log("alive cell will die from isolation");
+        } else if(number_of_live_neighbors === 2 || number_of_live_neighbors === 3) {
           nextGenStatus = true;
+          console.log("alive cell will survive");
         } else if(number_of_live_neighbors > 3) {
           nextGenStatus = false;
+          console.log("alive cell will die from overcrowding");
         }
-      } else if(cells[row2][col2].alive === false) {
+      } else if(cells[col2][row2].alive === false) {
         if(number_of_live_neighbors === 3) {
           nextGenStatus = true;
+          console.log("dead cell will regenerate");
         }
       }
-      cells[row2][col2].next_gen = nextGenStatus;
+      cells[col2][row2].next_gen = nextGenStatus;
     }
 
   }
  
 
 
-  function countNeighbors(r,c) {
+  function countNeighbors(c,r) {
 
     var neighbor_count = 0;
-    var x = r;
-    var y = c;
+    var x = c;
+    var y = r;
 
     if(x !== 0 && y !== 0) {
       if(cells[x-1][y-1].alive === true) {
